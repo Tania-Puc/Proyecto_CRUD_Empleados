14c095dbc2117baf847958983d76c27e178adacb
TaniaPuc <61524542+Tania-Puc@users.noreply.github.com>
2021-12-02 19:14:10 -0600

    cambio email test

--- test/validemail.test.js
+++ test/validemail.test.js
@@ -2,7 +2,7 @@
 //validar correo
 const validEmail =require('./validemail');
 
-test ('email taniamonserratpucpoot@gmail.com -> true',  ()=>{
+test ('email taniapuc@gmail.com -> true',  ()=>{
     expect(validEmail.isvalidEmail('taniapuc@gmail.com')).toBe(true)
 });
 
